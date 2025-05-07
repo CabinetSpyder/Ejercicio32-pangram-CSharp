816e23101d61448e7d88b073c425739bdb1076ed
¨CabinetSpyder¨ <andres.artaloytia@gmail.com>
2025-05-07 10:53:08 +0200

    Primera iteracion de la solucion

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,7 +1,27 @@
+using System.Text;
+
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+
+        if(string.IsNullOrWhiteSpace(input)){
+            return false;
+        }
+
+        List<char> auxStr = [];
+        input = input.ToLower();
+
+        foreach(char ch in input)
+        {
+            if(char.IsLetter(ch) && !auxStr.Contains(ch)){
+                auxStr.Add(ch);          
+            }
+
+        }
+
+        return auxStr.Count == 26;
+
+    
     }
 }
